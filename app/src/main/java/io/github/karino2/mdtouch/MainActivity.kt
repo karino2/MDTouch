3d850bf8e91f81b95615666dfc8cb5a4d6b90691
Kazuma Arino <hogeika2@gmail.com>
2021-05-09 22:23:53 +0900

    Initial commit.
    - H1, H2
    - unordered list
    - code span
    - code fence

---
+++ app/src/main/java/io/github/karino2/mdtouch/MainActivity.kt
@@ -0,0 +1,281 @@
+package io.github.karino2.mdtouch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import io.github.karino2.mdtouch.ui.theme.MDTouchTheme
+import org.intellij.markdown.MarkdownElementTypes
+import org.intellij.markdown.MarkdownTokenTypes
+import org.intellij.markdown.ast.ASTNode
+import org.intellij.markdown.ast.CompositeASTNode
+import org.intellij.markdown.ast.LeafASTNode
+import org.intellij.markdown.ast.getTextInNode
+import org.intellij.markdown.ast.impl.ListCompositeNode
+import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
+import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.parser.MarkdownParser
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        PrintHtml(md)
+        setContent {
+            MDTouchTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    RenderMd(md)
+                }
+            }
+        }
+    }
+}
+
+val md2 = """
+```
+code
+block
+```
+"""
+
+val md = """
+# H1 title
+
+brabra
+
+- hogehoge
+- ikaika
+   - fugafuga`tako`gyogyo
+
+after
+
+```
+code
+block
+```
+
+## H2 title
+
+abc
+
+"""
+
+fun PrintHtml(md: String) {
+    val flavour = GFMFlavourDescriptor()
+    val parser = MarkdownParser(flavour)
+
+    val tree = parser.buildMarkdownTreeFromString(md)
+    val html = HtmlGenerator(md, tree, flavour).generateHtml()
+    println(html)
+    /*
+            return HtmlGenerator(md, tree, flavour).generateHtml(
+
+     */
+}
+
+@Composable
+fun RenderMd(md: String){
+    val flavour = GFMFlavourDescriptor()
+    val parser = MarkdownParser(flavour)
+
+    val tree = parser.buildMarkdownTreeFromString(md)
+    RenderMarkdown(md, tree)
+}
+
+@Composable
+fun RenderMarkdown(md: String, root: ASTNode) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        if (root is CompositeASTNode) {
+            RenderBlocks(md, root)
+        }
+    }
+}
+
+@Composable
+fun RenderBlocks(md: String, blocks: CompositeASTNode) {
+    blocks.children.forEach { RenderBlock(md, it) }
+}
+
+@Composable
+fun RenderBox(content: AnnotatedString, style: TextStyle=LocalTextStyle.current) {
+    Box { Text(content,  style=style) }
+}
+
+@Composable
+fun RenderHeading(md: String, block: CompositeASTNode, style: TextStyle) {
+    RenderBox(buildAnnotatedString {
+               block.children.forEach { appendHeadingContent(md, it) }
+            }, style)
+}
+
+fun AnnotatedString.Builder.appendHeadingContent(md: String, node : ASTNode){
+    when(node.type) {
+        MarkdownTokenTypes.ATX_CONTENT -> {
+            appendTrimmingInline(md, node)
+            return
+        }
+    }
+    if (node is CompositeASTNode) {
+        node.children.forEach { appendHeadingContent(md, it) }
+        return
+    }
+}
+
+fun selectTrimmingInline(node: ASTNode) : List<ASTNode> {
+    val children = node.children
+    var from = 0
+    while (from < children.size && children[from].type == MarkdownTokenTypes.WHITE_SPACE) {
+        from++
+    }
+    var to = children.size
+    while (to > from && children[to - 1].type == MarkdownTokenTypes.WHITE_SPACE) {
+        to--
+    }
+
+    return children.subList(from, to)
+}
+
+
+fun AnnotatedString.Builder.appendInline(md: String, node : ASTNode, childrenSelector : (ASTNode)->List<ASTNode>){
+    val targets = childrenSelector(node)
+    targets.forEach {
+        if(it is LeafASTNode) {
+            append(it.getTextInNode(md).toString())
+        } else {
+            when(it.type) {
+                MarkdownElementTypes.CODE_SPAN -> {
+                    // val bgcolor = Color(0xFFF5F5F5)
+                    val bgcolor =  Color.LightGray
+                    pushStyle(SpanStyle(color=Color.Red, background = bgcolor))
+                    it.children.subList(1, it.children.size-1).forEach { item->
+                        append(item.getTextInNode(md).toString())
+                    }
+                    pop()
+                }
+
+            }
+        }
+    }
+}
+
+fun AnnotatedString.Builder.appendTrimmingInline(md: String, node : ASTNode){
+    appendInline(md, node, ::selectTrimmingInline)
+}
+
+@Composable
+fun RenderBlock(md: String, block: ASTNode) {
+    when(block.type) {
+        MarkdownElementTypes.ATX_1 -> {
+            RenderHeading(md, block as CompositeASTNode, MaterialTheme.typography.h1)
+        }
+        MarkdownElementTypes.ATX_2 -> {
+            RenderHeading(md, block as CompositeASTNode, MaterialTheme.typography.h2)
+        }
+        MarkdownElementTypes.ATX_3 -> {
+            RenderHeading(md, block as CompositeASTNode, MaterialTheme.typography.h3)
+        }
+        MarkdownElementTypes.ATX_4 -> {
+            RenderHeading(md, block as CompositeASTNode, MaterialTheme.typography.h4)
+        }
+        MarkdownElementTypes.ATX_5 -> {
+            RenderHeading(md, block as CompositeASTNode, MaterialTheme.typography.h5)
+        }
+        MarkdownElementTypes.ATX_6 -> {
+            RenderHeading(md, block as CompositeASTNode, MaterialTheme.typography.h6)
+        }
+        MarkdownElementTypes.PARAGRAPH -> {
+            RenderBox(buildAnnotatedString {
+                appendTrimmingInline(md, block)
+            })
+        }
+        MarkdownElementTypes.UNORDERED_LIST -> {
+            RenderUnorderedList(md, block as ListCompositeNode)
+        }
+        MarkdownElementTypes.CODE_FENCE -> {
+            RenderCodeFence(md, block)
+        }
+    }
+    println(block.type.name)
+}
+
+// similar to CodeFenceGeneratingProvider at GeneratingProviders.kt
+@Composable
+fun RenderCodeFence(md: String, node: ASTNode) {
+    Column (modifier = Modifier.background(Color.LightGray).padding(8.dp)) {
+        val codeStyle = TextStyle(fontFamily = FontFamily.Monospace)
+        val builder = StringBuilder()
+
+        var childrenToConsider = node.children
+        if (childrenToConsider.last().type == MarkdownTokenTypes.CODE_FENCE_END) {
+            childrenToConsider = childrenToConsider.subList(0, childrenToConsider.size - 1)
+        }
+
+        var lastChildWasContent = false
+
+        var renderStart = false
+        for (child in childrenToConsider) {
+            if (!renderStart && child.type == MarkdownTokenTypes.EOL) {
+                renderStart = true
+            }
+            else
+            {
+                when(child.type) {
+                    MarkdownTokenTypes.CODE_FENCE_CONTENT -> {
+                        builder.append(child.getTextInNode(md))
+                        lastChildWasContent = true
+                    }
+                    MarkdownTokenTypes.EOL -> {
+                        Text(style=codeStyle,
+                            text = builder.toString())
+                        builder.clear()
+                        lastChildWasContent = false
+                    }
+                }
+
+            }
+        }
+        if (lastChildWasContent) {
+            Text(style=codeStyle,
+                text = builder.toString())
+        }
+
+    }
+}
+
+@Composable
+fun RenderUnorderedList(md: String, list: ListCompositeNode) {
+    Column ( Modifier.offset(x = 15.dp) ) {
+        list.children.forEach { item->
+            if (item.type == MarkdownElementTypes.LIST_ITEM) {
+                Row {
+                    Canvas(modifier = Modifier.size(30.dp)) {
+                        drawCircle(radius=size.width/8, center=center, color= Color.Black) }
+                    Box {
+                        Column {
+                            RenderBlocks(md, item as CompositeASTNode)
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
